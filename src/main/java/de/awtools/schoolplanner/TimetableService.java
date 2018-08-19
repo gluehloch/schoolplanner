31b921889ad5c2888860f07e75968bd21ce6ca7d
andrewinkler <andre.winkler@web.de>
2018-08-19 13:33:20 +0200

    Some other API examples.

--- src/main/java/de/awtools/schoolplanner/TimetableService.java
+++ src/main/java/de/awtools/schoolplanner/TimetableService.java
@@ -3,6 +3,7 @@ package de.awtools.schoolplanner;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +30,27 @@ public class TimetableService {
 	// ------------------------------------------------------------------------
 
 	@CrossOrigin
-	@RequestMapping(value = "/courses/today", method = RequestMethod.GET)
-	public List<String> findAllSeason() {
+	@RequestMapping(value = "/school/{schoolId}/class/{classId}/course/today", method = RequestMethod.GET)
+	public List<String> findCoursesForToday(@PathVariable("schoolId") String schoolId,
+			@PathVariable("classId") String classId) {
 
-		// return betofficeBasicJsonService.findAllSeason();
 		return List.of("Andre", "Lars", "Adam", "Erwin", "Christine", "Spike");
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/school/{schoolId}/class/{classId}/course/week", method = RequestMethod.GET)
+	public List<String> findCoursesForWeek(@PathVariable("schoolId") String schoolId,
+			@PathVariable("classId") String classId) {
+
+		return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/school/{schoolId}/class/{classId}/course/{courseId}", method = RequestMethod.GET)
+	public List<String> findCourse(@PathVariable("schoolId") String schoolId, @PathVariable("classId") String classId,
+			@PathVariable("courseId") String courseId) {
+
+		return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
 	}
 
 }
