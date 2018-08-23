41d9777e65a56365bf31665e6f7ff1e230522129
Andre Winkler <gluehloch@googlemail.com>
2018-08-23 20:01:17 +0200

    Writing some tests to understand Spring and JUnit5.

--- domain/src/test/java/de/awtools/schoolplanner/school/SchoolClassRepositoryTest.java
+++ domain/src/test/java/de/awtools/schoolplanner/school/SchoolClassRepositoryTest.java
@@ -1,8 +1,24 @@
 package de.awtools.schoolplanner.school;
 
-import jdk.jfr.Timestamp;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@
+@ExtendWith(SpringExtension.class)
 public class SchoolClassRepositoryTest {
+
+    @Autowired
+    private SchoolClassRepository schoolClassRepository;
+    
+    @Test
+    public void findSchoolClass() {
+        SchoolClass schoolClass = new SchoolClass();
+        schoolClass.setName("5c");
+        schoolClass.setSchool(new School());
+        schoolClass.setTeacher(new Teacher());
+        schoolClass.setYear("2018/2019");
+        schoolClassRepository.save(schoolClass);
+    }
 
 }
