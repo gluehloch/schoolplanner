0c7bdba76eb0aed47666f7104da20c783dd83b04
Andre Winkler <andre.winkler@web.de>
2018-10-12 07:31:58 +0200

    Access token.

--- rest/src/main/java/de/awtools/schoolplanner/TimetableService.java
+++ rest/src/main/java/de/awtools/schoolplanner/TimetableService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,61 +18,69 @@ import de.awtools.schoolplanner.school.Lesson;
 import de.awtools.schoolplanner.school.LessonRepository;
 import de.awtools.schoolplanner.school.SchoolService;
 
-
 @RestController
 @RequestMapping("/timetable")
 public class TimetableService {
 
-	// ------------------------------------------------------------------------
-	// The beans
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // The beans
+    // ------------------------------------------------------------------------
 
+    // ------------------------------------------------------------------------
 
-	// ------------------------------------------------------------------------
+    @Autowired
+    private SchoolService schoolService;
 
-	@Autowired
-	private SchoolService schoolService;
-	
-	@Autowired
-	private LessonRepository lessonRepository;
+    @Autowired
+    private LessonRepository lessonRepository;
 
-	@CrossOrigin
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public Course createCourse(@RequestBody Course course) {
-		return course;
-	}
-	
-	@CrossOrigin
-	@GetMapping(value = "/school/{schoolId}/class/{classId}/course/today")
-	public List<Lesson> findCoursesForToday(@PathVariable("schoolId") String schoolId,
-			@PathVariable("classId") String classId) {
+    @CrossOrigin
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Course createCourse(@RequestBody Course course) {
+        return course;
+    }
 
-	    // Optional<SchoolClass> clazz = schoolService.findSchoolClass();
-	    
-		// classRepository.findById(4711L);
-		LocalDateTime now = LocalDateTime.now();
-		
-		// List<Lesson> lessons = lessonRepository.findToday(now.getDayOfWeek());
+    @CrossOrigin
+    @GetMapping(value = "/school/{schoolId}/class/{classId}/course/today")
+    public List<Lesson> findCoursesForToday(
+            @RequestHeader("token") String token,
+            @PathVariable("schoolId") String schoolId,
+            @PathVariable("classId") String classId) {
 
-		// return List.of("Andre", "Lars", "Adam", "Erwin", "Christine", "Spike");
-		
-		return null;
-	}
+        // TODO: Validate token, Validate access
+        
+        // Optional<SchoolClass> clazz = schoolService.findSchoolClass();
 
-	@CrossOrigin
-	@RequestMapping(value = "/school/{schoolId}/class/{classId}/course/week", method = RequestMethod.GET)
-	public List<String> findCoursesForWeek(@PathVariable("schoolId") String schoolId,
-			@PathVariable("classId") String classId) {
+        // classRepository.findById(4711L);
+        LocalDateTime now = LocalDateTime.now();
 
-		return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
-	}
+        // List<Lesson> lessons =
+        // lessonRepository.findToday(now.getDayOfWeek());
 
-	@CrossOrigin
-	@RequestMapping(value = "/school/{schoolId}/class/{classId}/course/{courseId}", method = RequestMethod.GET)
-	public List<String> findCourse(@PathVariable("schoolId") String schoolId, @PathVariable("classId") String classId,
-			@PathVariable("courseId") String courseId) {
+        // return List.of("Andre", "Lars", "Adam", "Erwin", "Christine",
+        // "Spike");
 
-		return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
-	}
+        return null;
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/school/{schoolId}/class/{classId}/course/week", method = RequestMethod.GET)
+    public List<String> findCoursesForWeek(
+            @PathVariable("schoolId") String schoolId,
+            @PathVariable("classId") String classId) {
+
+        return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag",
+                "Freitag");
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/school/{schoolId}/class/{classId}/course/{courseId}", method = RequestMethod.GET)
+    public List<String> findCourse(@PathVariable("schoolId") String schoolId,
+            @PathVariable("classId") String classId,
+            @PathVariable("courseId") String courseId) {
+
+        return List.of("Montag", "Dienstag", "Mittwoch", "Donnerstag",
+                "Freitag");
+    }
 
 }
